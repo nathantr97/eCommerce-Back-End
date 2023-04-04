b58bd23ee8b6f77fda15a9eac62fdf37af40dc3e
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-04-03 17:35:40 -0700

    debugged product api route

--- models/index.js
+++ models/index.js
@@ -13,13 +13,17 @@ Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+  onDelete: "cascade",
+})
+// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "tag_id",
   onDelete: "cascade",
 })
-// Tags belongToMany Products (through ProductTag)
-
 module.exports = {
   Product,
   Category,
